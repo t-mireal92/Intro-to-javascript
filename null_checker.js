89bd29c08514550a7963d57c0f4ef62f7f25b58f
t-mireal92 <mirielletush@gmail.com>
2019-06-14 09:41:10 +0300

    prompt

---
+++ null_checker.js
@@ -0,0 +1,10 @@
+for (m = 1; m<=10; m++) // for the loop to count ten times//
+{
+	let counter=prompt("input number:")// prompts the user to input number//
+	if (counter>0)
+		{console.log(1);}// displays a number greater than 0//
+	else if (counter<0)
+	 {console.log(-1);}//displays a number less than 0//
+	else
+	{console.log(0);}//displays 0 if the number is 0//
+}
